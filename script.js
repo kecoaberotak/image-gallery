8be1f01c4e9c6e9e85d9732b53805b93d4183488
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-06-27 19:24:38 +0700

    nambahin animasi pas ganti gambar

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const imageThumb = container.querySelectorAll('.thumbnail-image');
 
 container.addEventListener('click', function(image){
   if(image.target.className === 'thumbnail-image'){
+    // ada dua cara nangkepnya, sama aja
+    // imageSelected.src = image.target.src;
     imageSelected.setAttribute('src', image.target.src);
+
+    imageSelected.classList.add('fade');
+    setTimeout(function(){
+      imageSelected.classList.remove('fade');
+    }, 500);
   }
 });
